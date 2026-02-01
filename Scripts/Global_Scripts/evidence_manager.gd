6708077920fc781fec313b1bc27c9e75f4bb9394
GreyedOutName <yaboiwatson345@gmail.com>
2026-02-01 11:37:46 +0800

    masque changing and dialogue

--- Scripts/Global_Scripts/evidence_manager.gd
+++ Scripts/Global_Scripts/evidence_manager.gd
@@ -7,7 +7,6 @@ var Evidences:Dictionary ={
 	4:["The Heir","caught the Widow","giving poison"],#3
 	5:["He drank poison","willingly","to end the pain"],#3
 	6:["The Butler","hates","this place","if not for","the ring"],#3
-	7:["The Outsider","moved","the body"],#3
 	8:["The Heir","was here","during the death"],#3
 	9:["The Widow","did not know","it was poison"],#3
 	10:["The mansion","was never theirs"]#2
@@ -20,7 +19,6 @@ var Evidence_progress:Dictionary={
 	4:[0,0,0],
 	5:[0,0,0],
 	6:[0,0,0,0,0],
-	7:[0,0,0],
 	8:[0,0,0],
 	9:[0,0,0],
 	10:[0,0],
@@ -69,3 +67,5 @@ var NPC_State:Dictionary={
 	"Widow":"grey",
 	"Heir":"grey",
 }
+
+var has_key = false
